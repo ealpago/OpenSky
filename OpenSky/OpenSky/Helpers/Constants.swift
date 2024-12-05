6573460abf3eba41ffb17e8e0ed113f54c6dfd11
ealpago <alpagoemre@icloud.com>
2024-12-05 23:19:16 +0300

    Api constant added

---
+++ OpenSky/OpenSky/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  OpenSky
+//
+//  Created by Emre Alpago on 5.12.2024.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let shared = APIConstants()
+
+    let openSkyBaseURL = "https://opensky-network.org/api"
+}
