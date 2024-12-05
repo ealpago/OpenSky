b27f5a258899d31ba45f6bab1d1c750fcc7a6d20
ealpago <alpagoemre@icloud.com>
2024-12-05 20:37:18 +0300

    Project Created

---
+++ OpenSky/OpenSky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenSky
+//
+//  Created by Emre Alpago on 5.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
