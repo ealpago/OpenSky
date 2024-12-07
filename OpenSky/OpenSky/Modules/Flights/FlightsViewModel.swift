4ea442611b21a96f285141f8357db39480088daf
ealpago <alpagoemre@icloud.com>
2024-12-07 23:15:39 +0300

    Configuring to ViewModel and ViewController

--- OpenSky/OpenSky/Modules/Flights/FlightsViewModel.swift
+++ OpenSky/OpenSky/Modules/Flights/FlightsViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol FlightsViewModelInterface {}
+
+final class FlightsViewModel {
+
+    private weak var view: FlightsViewInterface?
+    private let networkManager: NetworkManager?
+
+    init(view: FlightsViewInterface?, networkManager: NetworkManager = NetworkManager.shared) {
+        self.view = view
+        self.networkManager = networkManager
+    }
+}
