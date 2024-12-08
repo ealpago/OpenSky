555e4240fd2120ecb17c57136111596a8e122f2a
ealpago <alpagoemre@icloud.com>
2024-12-08 12:53:26 +0300

    FlightVC now taking flights and show them

--- OpenSky/OpenSky/Modules/Flights/FlightsViewController.swift
+++ OpenSky/OpenSky/Modules/Flights/FlightsViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 import MapKit
 
-protocol FlightsViewInterface: AnyObject {
+protocol FlightsViewInterface: ProgressIndicatorPresentable {
+    var request: StatesRequest { get }
     func setupUI()
+    func addAnnotationsToMap(states: [FlightState])
+    func getCurrentState(from mapView: MKMapView) -> (lomin: Double, lamin: Double, lomax: Double, lamax: Double)
 }
 
 class FlightsViewController: UIViewController {
@@ -21,15 +24,63 @@ class FlightsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.viewDidLoad()
+        viewModel.viewDidLoad(request: request)
     }
 }
 
-extension FlightsViewController: MKMapViewDelegate {}
+extension FlightsViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let flightAnnotation = annotation as? FlightState else { return nil }
+
+        let identifier = "FlightAnnotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            annotationView?.annotation = annotation
+        }
+
+        annotationView?.image = UIImage(named: "plane_icon") // Optional: Custom pin image
+        return annotationView
+    }
+}
 
 extension FlightsViewController: FlightsViewInterface {
+    var request: StatesRequest {
+        let boundingBox = getCurrentState(from: mapView)
+        return StatesRequest(lomin: boundingBox.lomin, lamin: boundingBox.lamin, lomax: boundingBox.lomax, lamax: boundingBox.lamax)
+    }
+
     func setupUI() {
         mapView.delegate = self
         countriesButton.layer.cornerRadius = countriesButton.frame.height / 2
+    }
+
+    func addAnnotationsToMap(states: [FlightState]) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            mapView.removeAnnotations(mapView.annotations)
+            let annotations = states.compactMap { state -> FlightAnnotation? in
+                guard state.latitude != nil && state.longitude != nil else { return nil }
+                return FlightAnnotation(flightState: state)
+            }
+            mapView.addAnnotations(annotations)
+        }
+    }
+
+    func getCurrentState(from mapView: MKMapView) -> (lomin: Double, lamin: Double, lomax: Double, lamax: Double) {
+        let region = mapView.region
+        let centerLatitude = region.center.latitude
+        let centerLongitude = region.center.longitude
+        let latitudeDelta = region.span.latitudeDelta
+        let longitudeDelta = region.span.longitudeDelta
+        let lamin = centerLatitude - (latitudeDelta / 2)
+        let lamax = centerLatitude + (latitudeDelta / 2)
+        let lomin = centerLongitude - (longitudeDelta / 2)
+        let lomax = centerLongitude + (longitudeDelta / 2)
+        return (lomin, lamin, lomax, lamax)
     }
 }
--- OpenSky/OpenSky/Modules/Flights/FlightsViewModel.swift
+++ OpenSky/OpenSky/Modules/Flights/FlightsViewModel.swift
@@ -8,22 +8,46 @@
 import Foundation
 
 protocol FlightsViewModelInterface {
-    func viewDidLoad()
+    func viewDidLoad(request: StatesRequest)
+    func setMapView()
 }
 
 final class FlightsViewModel {
-
     private weak var view: FlightsViewInterface?
     private let networkManager: NetworkManager?
+    private var states: [FlightState] = []
 
     init(view: FlightsViewInterface?, networkManager: NetworkManager = NetworkManager.shared) {
         self.view = view
         self.networkManager = networkManager
     }
+
+    private func fetchStates(request: StatesRequest, completion: @escaping()->()) {
+        view?.showLoadingIndicator()
+        NetworkManager.shared.request(request: request, responseModel: StatesResponse.self) { [weak self] result in
+            guard let self = self else { return }
+            self.view?.hideLoadingIndicator()
+            switch result {
+            case .success(let response):
+                self.states = response.states
+                completion()
+            case .failure(let error):
+                print(error.self)
+            }
+        }
+    }
 }
 
 extension FlightsViewModel: FlightsViewModelInterface {
-    func viewDidLoad() {
+    func viewDidLoad(request: StatesRequest) {
         view?.setupUI()
+        fetchStates(request: request) { [weak self] in
+            guard let self = self else { return }
+            self.setMapView()
+        }
+    }
+
+    func setMapView() {
+        view?.addAnnotationsToMap(states: states)
     }
 }
